e830ff37d845857a39fddd3e869d2a185a61321a
blackDesertShinBeolUi <yanghye0321@naver.com>
2024-01-11 00:12:06 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="img/logo.jpg" />
+      
+          github pages hosting success....
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
